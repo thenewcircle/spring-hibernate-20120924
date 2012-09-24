8f9e7cdd9bba7318a9d1a6f66fbfa8b5f291b8ba
Dan Rosen <dan@marakana.com>
2012-09-24 15:43:24 -0700

    added hello world MVC demo

---
+++ src/main/java/com/marakana/contacts/servlets/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.marakana.contacts.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello-servlet")
+public class HelloWorld extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher view = request.getRequestDispatcher("jsp/hello.jsp");
+		view.forward(request, response);
+	}
+
+}
