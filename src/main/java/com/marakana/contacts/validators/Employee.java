4569f569ca26484641f72e98fb7692a01bbeabdc
Dan Rosen <dan@marakana.com>
2012-09-27 16:24:05 -0700

    added example validator

---
+++ src/main/java/com/marakana/contacts/validators/Employee.java
@@ -0,0 +1,14 @@
+package com.marakana.contacts.validators;
+
+import javax.validation.Constraint;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = EmployeeValidator.class)
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Employee {
+    String message() default "manager and employee must work for the same company";
+    Class<?>[] groups() default {};
+    Class<?>[] payload() default {};
+}
