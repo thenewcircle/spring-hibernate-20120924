3a536330e63b3898d02f99ce9da7f1c54cc68b8d
Dan Rosen <dan@marakana.com>
2012-09-27 15:55:01 -0700

    implemented manager / employer relationships

--- src/main/java/com/marakana/contacts/controllers/PersonController.java
+++ src/main/java/com/marakana/contacts/controllers/PersonController.java
@@ -2,6 +2,7 @@ package com.marakana.contacts.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.marakana.contacts.entities.Address;
 import com.marakana.contacts.entities.Person;
+import com.marakana.contacts.repositories.CompanyRepository;
 import com.marakana.contacts.repositories.PersonRepository;
 
 @Controller
@@ -16,6 +18,9 @@ public class PersonController {
 
 	@Autowired
 	private PersonRepository personRepository;
+
+	@Autowired
+	private CompanyRepository companyRepository;
 
 	@RequestMapping(value = "/person", params = "add", method = RequestMethod.GET)
 	public String getAddPerson() {
@@ -25,6 +30,8 @@ public class PersonController {
 	@RequestMapping(value = "/person", params = "edit", method = RequestMethod.GET)
 	public String getEditPerson(@RequestParam long id, Model model) {
 		model.addAttribute("person", personRepository.findOne(id));
+		model.addAttribute("managers", personRepository.findAll());
+		model.addAttribute("employers", companyRepository.findAll());
 		return "person/edit";
 	}
 
@@ -46,13 +53,18 @@ public class PersonController {
 	}
 
 	@RequestMapping(value = "/person", params = "edit", method = RequestMethod.POST)
+	@Transactional
 	public String postEditPerson(@RequestParam long id,
 			@RequestParam String name, @RequestParam String street,
 			@RequestParam String city, @RequestParam String state,
-			@RequestParam String zip) {
+			@RequestParam String zip,
+			@RequestParam("manager_id") long managerId,
+			@RequestParam("employer_id") long employerId) {
 		Person person = personRepository.findOne(id);
 		Address address = person.getAddress();
 		person.setName(name);
+		person.setManager(personRepository.findOne(managerId));
+		person.setEmployer(companyRepository.findOne(employerId));
 		address.setStreet(street);
 		address.setCity(city);
 		address.setState(state);
